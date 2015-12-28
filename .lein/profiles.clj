3fa3af906a3bb8077f7e16b1d280d7fa0c06e6ea
Jonathan Shieh <jonathan.shieh@gmail.com>
2015-12-28 10:54:33 +0800

    Updates .lein/profiles.clj to use latest nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.8.2"]
-            [org.clojure/tools.nrepl "0.2.7"]]}}
+ {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
