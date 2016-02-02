978fe27bb68ae9851ccae64216f7aedc02721a91
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-02-02 12:13:51 +0800

    Updates .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+ {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
+  :dependencies [[alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]]}}
