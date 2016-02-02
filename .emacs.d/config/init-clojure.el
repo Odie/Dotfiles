6dfdf2af44dafbac7d67e832f963d4c54de99784
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-02-02 12:16:18 +0800

    Switches over from emacs to spacemacs

--- .emacs.d/config/init-clojure.el
+++
@@ -1,7 +0,0 @@
-(use-package clojure-mode
-	:mode (("\\.boot\\'" . clojure-mode))
-
-	:config
-	(progn
-		(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
-		(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)))
--- .emacs.d/config/init-org.el
+++
@@ -1,5 +0,0 @@
-; Org mode should always wrap at window edge
-(setq org-startup-truncated nil)
-
-(provide 'init-org)
-;;; init-org.el ends here
