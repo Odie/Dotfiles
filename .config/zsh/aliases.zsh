0f3597ea0515b6d108d07c023faf494c5ae210e0
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-07-05 15:44:07 +0800

    Adds aliases for yazi

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -67,3 +67,12 @@ function phpunit()
 {
 	`git-root`/vendor/bin/phpunit -c `git-root`/phpunit.xml
 }
+
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
