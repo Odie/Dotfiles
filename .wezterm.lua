e20033df7ff5451164367ab4711fb3d6d8f8967d
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-08-12 12:37:42 +0800

    Adds wezterm shortcuts to increase and decresase font sizes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,21 +1,33 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local mykeys = {}
+local keybinds = {
+	{
+		key = "+",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.IncreaseFontSize,
+	},
+	{
+		key = "-",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.DecreaseFontSize,
+	},
+}
+
 for i = 1, 8 do
-  -- CTRL+ALT + number to move to that position
-  table.insert(mykeys, {
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = wezterm.action.MoveTab(i - 1),
-  })
+	-- CTRL+ALT + number to move to that position
+	table.insert(keybinds, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
 end
 
 return {
-  initial_rows = 100,
-  initial_cols = 1000,
-  -- window_decorations = "NONE",
-  font_size = 14,
-  color_scheme = "Gruvbox dark, medium (base16)",
-  keys = mykeys,
-  window_decorations = "RESIZE",
+	initial_rows = 100,
+	initial_cols = 1000,
+	-- window_decorations = "NONE",
+	font_size = 14,
+	color_scheme = "Gruvbox dark, medium (base16)",
+	keys = keybinds,
+	window_decorations = "RESIZE",
 }
